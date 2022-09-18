15b2e6543cde72275d9f4fbecaf8353ef8bed6df
guillaume <reygner0224@gmail.com>
2022-09-18 18:21:18 +0200

    add URL for Access COntrol Allow Origin

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://reservation-app-ruddy.vercel.app"), 
+  res.header("Access-Control-Allow-Origin", "https://reservation-app-liard.vercel.app"), 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
   next()
 })
